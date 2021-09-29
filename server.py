a7955ac1bbdfa820356e00ed6559b01681970c03
Xu <dao-jun.xu@accenturefederal.com>
2021-09-29 11:41:11 -0400

    added feature toggle

--- server.py
+++ server.py
@@ -214,7 +214,11 @@ def show_locations():
     """Show local pickup locations"""
 
     pickups = db.session.query(Pickup).filter(Pickup.pickup_id > 1).all()
-    return render_template("locations.html", pickups=pickups)
+    if os.environ["LOCATION_FEATURE_ENABLED"] == "True":
+        pickups = db.session.query(Pickup).filter(Pickup.pickup_id > 1).all()
+        return render_template("locations.html", pickups=pickups)
+    else:
+        return render_template("coming-soon.html")
 
 
 @app.route('/cart')
@@ -222,7 +226,7 @@ def show_cart():
     """Query session for cart contents and display results"""
     
     # send flag for feature toggle to cart page
-    return render_template("cart.html")
+    return render_template("cart.html", flag=os.environ["LOCATION_FEATURE_ENABLED"])
 
 
 @app.route('/cart', methods=['POST'])
