49d0fd3a000ebcfa391d03736b18851af957039b
Invernizzi <lucas.invernizzi@accenturefederal.com>
2021-10-07 10:15:22 -0400

    added unit tests

--- unittests.py
+++ unittests.py
@@ -1,84 +1,43 @@
 import unittest
-import flask
-import api
-import functions
-import model
-import server
+from api import split_params, get_recipes
+import json
+import os
 
 
-class TestName(unittest.TestCase):
-
-    def setUp(self):
-        """Method to prepare the test fixture. Run BEFORE the test methods."""
-        self.client = server.app.test_client()
-        server.app.config['TESTING'] = True
-        # Connect to test database
-        model.connect_to_db(server.app)
-
-        # Create tables and add sample data
-        model.db.create_all()
-        model.example_data()
-        self.cart = model.Product.query.filter(model.Product.product_id.in_(flask.session['cart'].keys())).all()
-        pass
-
-    def tearDown(self):
-        """Method to tear down the test fixture. Run AFTER the test methods."""
-        model.db.session.close()
-        model.db.drop_all()
-        pass
-
-    def testAPI(self):
-        self.assertEqual(api.split_params([]), [])
-        self.assertEqual(api.split_params(['']), [[]])
-        self.assertEqual(api.split_params(['Organic Food', 'Fresh Food', 'Food (Frozen)', 'Pre-Washed Food']),
+class UnitTests(unittest.TestCase):
+    def test_split_params(self):
+        with self.assertRaises(TypeError) as cm:
+            split_params()
+        with self.assertRaises(TypeError) as cm:
+            split_params(1)
+        with self.assertRaises(AttributeError) as cm:
+            split_params([[]])
+        with self.assertRaises(AttributeError) as cm:
+            split_params([['Food']])
+        self.assertEqual(split_params(''), [])
+        self.assertEqual(split_params('Food'), [['F'], ['o'], ['o'], ['d']])
+        self.assertEqual(split_params([]), [])
+        self.assertEqual(split_params(['']), [[]])
+        self.assertEqual(split_params(['Organic Food', 'Fresh Food', 'Food (Frozen)', 'Pre-Washed Food']),
                          [['Food'], ['Food'], ['Food'], ['Food']])
-        self.assertEqual(api.split_params([item.name for item in self.cart]), [['Blackberries']])
+        self.assertEqual(split_params(['Blackberries']), [['Blackberries']])
 
-        self.assertEqual(api.get_recipes([]), [])
-        self.assertEqual(api.get_recipes([['']]), [])
-        self.assertIn('Pickled Blackberries', api.get_recipes([['Blackberries']]))
-
-    def testFunctions(self):
-        self.assertEqual(functions.get_cart_weight(self.cart), 6)
-        self.assertEqual(functions.get_cart_total(self.cart), 3.99)
-
-    def testServer(self):
-        result = self.client.get('/register')
-        self.assertIn(b'register_img', result.data)
-
-        result = self.client.post('/products/1', data={'productID': '1'}, follow_redirects=True)
-        self.assertIn(b'Blackberries', result.data)
-
-        result = self.client.get('/account')
-        self.assertIn(b'Jane Doe', result.data)
-
-        result = self.client.post('/cart',
-                                  data={'delivery': 'delivery',
-                                        'address': {'street': '1234 Main Street',
-                                                    'zipcode': '31626'}},
-                                  follow_redirects=True)
-        self.assertIn(b'Success', result.data)
-
-        result = self.client.post('/save-recipe',
-                                  data={'url': 'http://foodandstyle.com/2012/12/20/persimmon-cosmopolitan/'},
-                                  follow_redirects=True)
-        self.assertIn(b'Success', result.data)
-
-        result = self.client.get('/checkout')
-        self.assertIn(b'Thank you', result.data)
-
-        result = self.client.post('/checkout')
-        self.assertIn(b'Your order has been placed!', result.data)
-
-        result = self.client.post('/add-item', data={'product_id': 1}, follow_redirects=True)
-        self.assertIn(b'Success!', result.data)
-
-        result = self.client.post('/update-cart', data={'product_id': 1, 'qty': 3})
-        self.assertIn(b'Success', result.data)
+    def test_get_recipes(self):
+        with self.assertRaises(TypeError) as cm:
+            get_recipes()
+        with self.assertRaises(TypeError) as cm:
+            get_recipes(1)
+        with self.assertRaises(TypeError) as cm:
+            get_recipes([1])
+        with self.assertRaises(TypeError) as cm:
+            get_recipes([[1]])
+        self.assertEqual(get_recipes([]), [])
+        self.assertEqual(get_recipes([['']]), [])
+        self.assertIn('Pickled Blackberries', json.dumps(get_recipes([['Blackberries']])))
 
 
 if __name__ == "__main__":
-    log_file = 'log_file.txt'
+    log_file = os.environ['LOG_FILE_NAME']
     with open(log_file, "w") as f:
         runner = unittest.TextTestRunner(f)
         unittest.main(testRunner=runner)
